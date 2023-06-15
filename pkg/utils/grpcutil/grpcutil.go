6825daf4723ca4a3d92f2011227a36aa4a2d8efc
Ryan Leung <rleungx@gmail.com>
2023-06-15 18:13:10 +0800

    *: extract conn and replace `GetAllocator` (#6557)
    ref tikv/pd#5839, ref tikv/pd#6553
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- pkg/utils/grpcutil/grpcutil.go
+++ pkg/utils/grpcutil/grpcutil.go
@@ -24,17 +24,21 @@ import (
 	"time"
 
 	"github.com/pingcap/log"
+	"github.com/pkg/errors"
 	"github.com/tikv/pd/pkg/errs"
 	"github.com/tikv/pd/pkg/utils/logutil"
 	"go.etcd.io/etcd/pkg/transport"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
 )
 
-// ForwardMetadataKey is used to record the forwarded host of PD.
-const ForwardMetadataKey = "pd-forwarded-host"
+const (
+	// ForwardMetadataKey is used to record the forwarded host of PD.
+	ForwardMetadataKey = "pd-forwarded-host"
+)
 
 // TLSConfig is the configuration for supporting tls.
 type TLSConfig struct {
@@ -191,4 +195,43 @@ func NeedRebuildConnection(err error) bool {
 		strings.Contains(err.Error(), codes.Unknown.String()) || // Unknown error.
 		strings.Contains(err.Error(), codes.ResourceExhausted.String()) // ResourceExhausted is returned when either the client or the server has exhausted their resources.
 	// Besides, we don't need to rebuild the connection if the code is Canceled, which means the client cancelled the request.
+}
+
+func establish(ctx context.Context, addr string, tlsConfig *TLSConfig, do ...grpc.DialOption) (*grpc.ClientConn, error) {
+	tlsCfg, err := tlsConfig.ToTLSConfig()
+	if err != nil {
+		return nil, err
+	}
+	cc, err := GetClientConn(
+		ctx,
+		addr,
+		tlsCfg,
+		do...,
+	)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return cc, nil
+}
+
+// CreateClientConn creates a client connection to the given target.
+func CreateClientConn(ctx context.Context, addr string, tlsConfig *TLSConfig, do ...grpc.DialOption) *grpc.ClientConn {
+	var (
+		conn *grpc.ClientConn
+		err  error
+	)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+		}
+		conn, err = establish(ctx, addr, tlsConfig, do...)
+		if err != nil {
+			log.Error("cannot establish connection", zap.String("addr", addr), errs.ZapError(err))
+			continue
+		}
+		break
+	}
+	return conn
 }
