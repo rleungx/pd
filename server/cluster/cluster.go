ad27babbc0c21f54a8b8c14cb43df19aabefb70b
Ryan Leung <rleungx@gmail.com>
2023-07-12 17:56:13 +0800

    mcs: add scheduling microservice framework (#6542)
    ref tikv/pd#5839
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/cluster/cluster.go
+++ server/cluster/cluster.go
@@ -593,6 +593,7 @@ func (c *RaftCluster) runUpdateStoreStats() {
 	}
 }
 
+// runCoordinator runs the main scheduling loop.
 func (c *RaftCluster) runCoordinator() {
 	defer logutil.LogPanic()
 	defer c.wg.Done()
