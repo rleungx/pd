0604d496c911330cd0310ba584a898cfe3ed869b
lhy1024 <admin@liudos.us>
2023-11-20 17:46:10 +0800

    mcs: fix scheduler memory sync in api server (#7389)
    close tikv/pd#7388
    
    Signed-off-by: lhy1024 <admin@liudos.us>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- pkg/schedule/schedulers/evict_slow_store_test.go
+++ pkg/schedule/schedulers/evict_slow_store_test.go
@@ -120,12 +120,12 @@ func (suite *evictSlowStoreTestSuite) TestEvictSlowStorePrepare() {
 	suite.True(ok)
 	suite.Zero(es2.conf.evictStore())
 	// prepare with no evict store.
-	suite.es.Prepare(suite.tc)
+	suite.es.PrepareConfig(suite.tc)
 
 	es2.conf.setStoreAndPersist(1)
 	suite.Equal(uint64(1), es2.conf.evictStore())
 	// prepare with evict store.
-	suite.es.Prepare(suite.tc)
+	suite.es.PrepareConfig(suite.tc)
 }
 
 func (suite *evictSlowStoreTestSuite) TestEvictSlowStorePersistFail() {
